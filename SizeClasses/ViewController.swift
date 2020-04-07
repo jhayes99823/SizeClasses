3f5414e73e38740cc1e452fc47d71e43068e4872
Jordan Hayes <hayesja99@gmail.com>
2020-04-07 18:54:17 -0400

    Initial Commit

---
+++ SizeClasses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SizeClasses
+//
+//  Created by CSSE Department on 4/7/20.
+//  Copyright © 2020 CSSE Department. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
